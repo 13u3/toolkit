5c498c09a6486ee834de3ceb92cc23bffb758068
zhang xinyang <39096226@qq.com>
2024-03-25 13:28:45 +0800

    Init

--- config/etcd/etcd.go
+++ config/etcd/etcd.go
@@ -1,22 +1,16 @@
 package etcd
 
 import "time"
-type Config struct {
-    Endpoints []string
+
+type DefaultConfig struct {
+	Endpoints   []string
 	DialTimeout time.Duration
 }
 
-func NewConfig(endpoints []string, dialTimeout time.Duration) *Config {
-    return &Config{
-        Endpoints: endpoints,
-        DialTimeout: dialTimeout,
-    }
+// 初始化配置
+func NewConfig(endpoints []string, dialTimeout time.Duration) *DefaultConfig {
+	return &DefaultConfig{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	}
 }
-
-func (c *Config) WithDefaults() *Config {
-    if c.DialTimeout == 0 {
-        c.DialTimeout = 5 * time.Second // 默认5秒
-    }
-    return c
-}
-
